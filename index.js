e481e21d8ce557df2362d3821d5ca8a5e34bdc8d
Vishwas Acharya <vishwasracharya@gmail.com>
2022-02-11 11:43:25 +0530

    Bootstrap Added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.set("view engine", "ejs");
-
-app.get("/", (req, res) => {
-    res.render("index");
-});
-
-app.listen(3000);
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
