5606f6a91ef010fbed6de37bc1565b9327d2c231
Vishwas Acharya <vishwasracharya@gmail.com>
2022-02-26 23:45:49 +0530

    added dark mode with local storage

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,10 +1,33 @@
-// 💡 Dark & Light Mode Toggle 
-function theme() {
-    let element = document.body;
-    element.classList.toggle("dark-mode");
-    let navbar = document.getElementById("navbar")
-    navbar.classList.toggle('navbar-dark');
-    navbar.classList.toggle('bg-dark');
-    navbar.classList.toggle('shadow-white');
-    document.getElementById('github').classList.toggle('invert');
+// 💡 Dark & Light Mode Toggle Using Local Storage
+let darkMode = localStorage.getItem('darkMode');
+const darkModeToggle = document.querySelector('#darkModeToggle');
+const enableDarkMode = () => {
+    document.body.classList.add('dark-mode');
+    document.getElementById('homeNavbar').classList.remove('bg-white');
+    document.getElementById('homeNavbar').classList.remove('navbar-light');
+    document.getElementById('homeNavbar').classList.add('navbar-dark');
+    document.getElementById('homeNavbar').classList.add('bg-dark');
+    localStorage.setItem('darkMode', 'enabled');
 }
+const disableDarkMode = () => {
+    document.body.classList.remove('dark-mode');
+    document.getElementById('homeNavbar').classList.add('bg-white');
+    document.getElementById('homeNavbar').classList.add('navbar-light');
+    document.getElementById('homeNavbar').classList.remove('navbar-dark');
+    document.getElementById('homeNavbar').classList.remove('bg-dark');
+    localStorage.setItem('darkMode', null);
+}
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+darkModeToggle.addEventListener('click', () => {
+    console.log('clicked');
+    darkMode = localStorage.getItem('darkMode');
+    if (darkMode === 'enabled') {
+        console.log('disableDarkMode');
+        disableDarkMode();
+    } else {
+        console.log('enableDarkMode');
+        enableDarkMode();
+    }
+});
