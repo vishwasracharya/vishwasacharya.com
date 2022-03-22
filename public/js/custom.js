af1638af787a6bd205196b8c8a5ccd14f9b44c51
Vishwas Acharya <vishwasracharya@gmail.com>
2022-03-22 20:03:27 +0530

    General Updates

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,39 +1,52 @@
-// 💡 Dark & Light Mode Toggle Using Local Storage
-let darkMode = localStorage.getItem('darkMode');
-const darkModeToggle = document.querySelector('#darkModeToggle');
-const enableDarkMode = () => {
-    document.body.classList.add('dark-mode');
-    document.getElementById('homeNavbar').classList.remove('bg-white');
-    document.getElementById('homeNavbar').classList.remove('navbar-light');
-    document.getElementById('homeNavbar').classList.add('navbar-dark');
-    document.getElementById('homeNavbar').classList.add('bg-dark');
-    document.getElementsByClassName('badge')[0].classList.remove('bg-light');
-    document.getElementsByClassName('badge')[0].classList.add('bg-dark');
-    document.getElementsByClassName('badge')[0].classList.add('text-white');
-    localStorage.setItem('darkMode', 'enabled');
-}
-const disableDarkMode = () => {
-    document.body.classList.remove('dark-mode');
-    document.getElementById('homeNavbar').classList.add('bg-white');
-    document.getElementById('homeNavbar').classList.add('navbar-light');
-    document.getElementById('homeNavbar').classList.remove('navbar-dark');
-    document.getElementById('homeNavbar').classList.remove('bg-dark');
-    document.getElementsByClassName('badge')[0].classList.remove('bg-dark');
-    document.getElementsByClassName('badge')[0].classList.add('bg-light');
-    document.getElementsByClassName('badge')[0].classList.remove('text-white');
-    localStorage.setItem('darkMode', null);
-}
-if (darkMode === 'enabled') {
-    enableDarkMode();
-}
-darkModeToggle.addEventListener('click', () => {
-    darkMode = localStorage.getItem('darkMode');
-    if (darkMode === 'enabled') {
-        disableDarkMode();
+const site_url = window.location.href;
+function navbarActive() {
+    if (site_url.includes("/about")) {
+        document.getElementsByClassName("tab")[1].classList.add("tab-active");
+    } else if (site_url.includes("/blog")) {
+        document.getElementsByClassName("tab")[2].classList.add("tab-active");
+    } else if (site_url.includes("/podcast")) {
+        document.getElementsByClassName("tab")[3].classList.add("tab-active");
     } else {
-        enableDarkMode();
+        document.getElementsByClassName("tab")[0].classList.add("tab-active");
     }
-});
+}
+navbarActive();
+// 💡 Dark & Light Mode Toggle Using Local Storage
+// let darkMode = localStorage.getItem('darkMode');
+// const darkModeToggle = document.querySelector('#darkModeToggle');
+// const enableDarkMode = () => {
+//     document.body.classList.add('dark-mode');
+//     document.getElementById('homeNavbar').classList.remove('bg-white');
+//     document.getElementById('homeNavbar').classList.remove('navbar-light');
+//     document.getElementById('homeNavbar').classList.add('navbar-dark');
+//     document.getElementById('homeNavbar').classList.add('bg-dark');
+//     document.getElementsByClassName('badge')[0].classList.remove('bg-light');
+//     document.getElementsByClassName('badge')[0].classList.add('bg-dark');
+//     document.getElementsByClassName('badge')[0].classList.add('text-white');
+//     localStorage.setItem('darkMode', 'enabled');
+// }
+// const disableDarkMode = () => {
+//     document.body.classList.remove('dark-mode');
+//     document.getElementById('homeNavbar').classList.add('bg-white');
+//     document.getElementById('homeNavbar').classList.add('navbar-light');
+//     document.getElementById('homeNavbar').classList.remove('navbar-dark');
+//     document.getElementById('homeNavbar').classList.remove('bg-dark');
+//     document.getElementsByClassName('badge')[0].classList.remove('bg-dark');
+//     document.getElementsByClassName('badge')[0].classList.add('bg-light');
+//     document.getElementsByClassName('badge')[0].classList.remove('text-white');
+//     localStorage.setItem('darkMode', null);
+// }
+// if (darkMode === 'enabled') {
+//     enableDarkMode();
+// }
+// darkModeToggle.addEventListener('click', () => {
+//     darkMode = localStorage.getItem('darkMode');
+//     if (darkMode === 'enabled') {
+//         disableDarkMode();
+//     } else {
+//         enableDarkMode();
+//     }
+// });
 
 function calculateAge(fullbirthday) {
     var today = new Date();
