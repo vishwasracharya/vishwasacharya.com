a6fd4313554e5b8c7ee5fe2b4721ce964ab5eeda
Vishwas Acharya <vishwasracharya@gmail.com>
2022-04-28 22:03:15 +0530

    Added more dependancies

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const path = require('path');
+const express = require('express'),
+      dotenv = require('dotenv'),
+      path = require('path'),
+      compression = require('compression'),
+      bodyParser = require('body-parser'),
+      helmet = require('helmet');
+
 const PORT = process.env.PORT || 5000
 
 dotenv.config({ path: './config/config.env' });
@@ -8,19 +12,32 @@ require('./db/conn');
 const Subscriber = require('./model/subscriberSchema');
 
 // Routers for the different pages
-let indexRouter = require('./routes/index');
-let blogRouter = require('./routes/blog');
-let podcastRouter = require('./routes/podcast');
+let indexRouter = require('./routes/index'),
+    blogRouter = require('./routes/blog'),
+    podcastRouter = require('./routes/podcast');
 
 let app = express();
 app.use(express.json());
-
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true,
+  })
+);
 
 app
   .use(express.static(path.join(__dirname, 'public')))
+  .use(compression())
+  .use(helmet())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .listen(PORT, () => console.log(`App Started on ${ PORT }`))
+  
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'public, max-age=31536000');
+    next();
+});
 
 app.use('/', indexRouter)
 app.use('/blog', blogRouter)
