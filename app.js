fbc25bfa80e497cbe3ab7c2eadc7100b8d133980
Vishwas Acharya <vishwasracharya@gmail.com>
2022-05-18 14:41:57 +0530

    Added more dependancy update sitemap

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const express = require('express'),
-      dotenv = require('dotenv'),
-      path = require('path'),
-      compression = require('compression'),
-      bodyParser = require('body-parser'),
-      helmet = require('helmet');
+const express = require('express');
+
+const cookieParser = require("cookie-parser");
+const createError = require("http-errors");
+const compression = require('compression');
+const bodyParser = require('body-parser');
+const minify = require("express-minify");
+const favicon = require("serve-favicon");
+const keys = require('./config/keys');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
+const cors = require("cors");
+const path = require('path');
 
 const PORT = process.env.PORT || 5000
 
@@ -11,12 +18,31 @@ dotenv.config({ path: './config/config.env' });
 require('./db/conn');
 
 // Routers for the different pages
-let indexRouter = require('./routes/index'),
-    blogRouter = require('./routes/blog'),
-    podcastRouter = require('./routes/podcast');
+let indexRouter = require('./routes/index');
+let blogRouter = require('./routes/blog');
+let podcastRouter = require('./routes/podcast');
 
 let app = express();
+
+// view engine setup
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(cors());
+
+app.set("trust proxy", 1);
+
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use(compression());
+// app.use(minify());
 app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -25,13 +51,10 @@ app.use(
   })
 );
 
-app
-  .use(express.static(path.join(__dirname, 'public')))
-  .use(compression())
-  // .use(helmet())
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .listen(PORT, () => console.log(`App Started on ${ PORT }`))
+app.use(cookieParser());
+// app.use(helmet());
+
+app.listen(PORT, () => console.log(`App Started on ${ PORT }`))
   
 app.use((req, res, next) => {
     res.set('Cache-Control', 'public, max-age=31536000');
@@ -41,5 +64,17 @@ app.use((req, res, next) => {
 app.use('/', indexRouter)
 app.use('/blog', blogRouter)
 app.use('/podcast', podcastRouter)
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.render("error", { env: keys.environment });
+});
 
 module.exports = app;
--- config/keys.js
+++ config/keys.js
@@ -1,1 +1,2 @@
+exports.environment = process.env.environment || "dev";
 exports.site_url = process.env.SITE_URL || 'http://localhost:5000';
